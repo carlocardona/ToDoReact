de87f5f6185e982e188c7ae4214fb5db1b0f3550
Carlo Cardona <carlo.dev2@gmail.com>
2021-03-04 22:09:52 +1100

    added pull down and input

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './components/Form'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>To Do List</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
